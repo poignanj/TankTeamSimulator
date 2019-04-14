7e01d426b8414f8b58a2abb4e0993773b7652448
Unknown <camille.chastain@insa-cvl.fr>
2019-04-14 19:38:21 -0400

    Ammo, Sons et Cie

--- Assets/__Scripts/inputs/PilotController.cs
+++ Assets/__Scripts/inputs/PilotController.cs
@@ -4,6 +4,14 @@ using UnityEngine.Experimental.Input;
 
 public class PilotController : MonoBehaviour, IPilotTankActions
 {
+
+    public AudioSource m_MovementAudio;         // Reference to the audio source used to play engine sounds. NB: different to the shooting audio source.
+    public AudioClip m_EngineIdling;            // Audio to play when the tank isn't moving.
+    public AudioClip m_EngineDriving;           // Audio to play when the tank is moving.
+    private float m_OriginalPitch;              // The pitch of the audio source at the start of the scene.
+    public float m_PitchRange = 0.2f;           // The amount by which the pitch of the engine noises can vary.
+
+
     [SerializeField] private InputMaster inputMaster;
     [SerializeField] public Transform leftTrack, rightTrack;
     [Space][SerializeField] private float leftSpeed = 0, rightSpeed = 0;
@@ -12,6 +20,12 @@ public class PilotController : MonoBehaviour, IPilotTankActions
     {
         inputMaster.PilotTank.SetCallbacks(this);
         engine = this.GetComponent<Rigidbody>();
+    }
+
+    private void Start()
+    {
+        // Store the original pitch of the audio source.
+        m_OriginalPitch = m_MovementAudio.pitch;
     }
 
     private void OnEnable()
@@ -27,7 +41,8 @@ public class PilotController : MonoBehaviour, IPilotTankActions
         //todo: Adjust speed scaling (percentile?)
         engine.velocity.Set(0, 0, 0);
         engine.AddForceAtPosition(this.transform.forward.normalized * leftSpeed, leftTrack.position);
-        engine.AddForceAtPosition(this.transform.forward.normalized * rightSpeed, rightTrack.position);
+        engine.AddForceAtPosition(this.transform.forward.normalized * rightSpeed, rightTrack.position);            EngineAudio ();
+        EngineAudio();
     }
 
     public void OnMoveLeftLever(InputAction.CallbackContext context)
@@ -42,4 +57,32 @@ public class PilotController : MonoBehaviour, IPilotTankActions
         rightSpeed += direction;
     }
 
+
+    private void EngineAudio()
+    {
+        Debug.Log(engine.velocity);
+        // If there is no input (the tank is stationary)...
+        if (Mathf.Abs(engine.velocity.x)<0.2 && Mathf.Abs(engine.velocity.y) < 0.2 && Mathf.Abs(engine.velocity.z) < 0.2)
+        {
+            // ... and if the audio source is currently playing the driving clip...
+            if (m_MovementAudio.clip == m_EngineDriving)
+            {
+                // ... change the clip to idling and play it.
+                m_MovementAudio.clip = m_EngineIdling;
+                m_MovementAudio.pitch = Random.Range(m_OriginalPitch - m_PitchRange, m_OriginalPitch + m_PitchRange);
+                m_MovementAudio.Play();
+            }
+        }
+        else
+        {
+            // Otherwise if the tank is moving and if the idling clip is currently playing...
+            if (m_MovementAudio.clip == m_EngineIdling)
+            {
+                // ... change the clip to driving and play.
+                m_MovementAudio.clip = m_EngineDriving;
+                m_MovementAudio.pitch = Random.Range(m_OriginalPitch - m_PitchRange, m_OriginalPitch + m_PitchRange);
+                m_MovementAudio.Play();
+            }
+        }
+    }
 }
