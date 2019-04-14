28c8f063c299e7b8da53b8be479f9824bad96877
Illis Draklaw <jean_jacques.poignant@utt.fr>
2019-04-14 17:16:39 -0400

    startArtillery

--- Assets/__Scripts/inputs/ArtilleryController.cs
+++ Assets/__Scripts/inputs/ArtilleryController.cs
@@ -5,15 +5,41 @@ using UnityEngine.Experimental.Input;
 
 public class ArtilleryController : MonoBehaviour, IArtilleryActions
 {
+    [SerializeField] private InputMaster inputMaster;
+    [SerializeField] private GameObject turret;
+    [Space] [SerializeField] private float turnIncrement = 0, aimIncrement = 0;
+
+    private void Awake()
+    {
+        inputMaster.Artillery.SetCallbacks(this);
+    }
+
+    private void OnEnable()
+    {
+        inputMaster.Enable();
+    }
+    private void OnDisable()
+    {
+        inputMaster.Disable();
+    }
+
+    private void Update()
+    {
+        turret.transform.Rotate(new Vector3(0, 0, 1), turnIncrement);
+    }
     public void OnRotateX(InputAction.CallbackContext context)
     {
         //Todo: Rotate turret (left&right)
+        var direction = context.ReadValue<Vector2>();
         //Warning: Calculate rotation first (stick from gamepad)
+        turnIncrement = direction[0];
     }
 
     public void OnRotateY(InputAction.CallbackContext context)
     {
         //Todo: Rotate canon (up&down)
+        var direction = context.ReadValue<Vector2>();
         //Warning: Calculate rotation first (stick from gamepad)
+        aimIncrement = direction[0];
     }
 }
--- Assets/__Scripts/inputs/ChiefController.cs
+++ Assets/__Scripts/inputs/ChiefController.cs
@@ -5,6 +5,22 @@ using UnityEngine.Experimental.Input;
 
 public class ChiefController : MonoBehaviour, IChiefControlsActions
 {
+    [SerializeField] private InputMaster inputMaster;
+
+    private void Awake()
+    {
+        inputMaster.ChiefControls.SetCallbacks(this);
+    }
+
+    private void OnEnable()
+    {
+        inputMaster.Enable();
+    }
+    private void OnDisable()
+    {
+        inputMaster.Disable();
+    }
+
     public void OnClick(InputAction.CallbackContext context)
     {
         //Todo: Send message (distance & direction) to Artillery
