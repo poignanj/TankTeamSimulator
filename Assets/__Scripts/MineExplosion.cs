21ec040d441110d0246c5fc1b1bbe873c6a091fa
Unknown <camille.chastain@insa-cvl.fr>
2019-04-22 01:12:39 -0400

    UNE MINE

---
+++ Assets/__Scripts/MineExplosion.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MineExplosion : MonoBehaviour
+{
+
+    public float m_ExplosionRadius = 2f;                // The maximum distance away from the explosion tanks can be and are still affected.
+    public GameObject ExplosionPrefab;
+
+    private ParticleSystem m_ExplosionParticles;         // Reference to the particles that will play on explosion.
+    private AudioSource m_ExplosionAudio;                // Reference to the audio that will play on explosion.
+
+
+    void Start()
+    {
+
+    }
+    
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Tank")
+        {
+            Debug.Log("Y'a eu trigger avec " + collision.collider.name);
+            Collider[] colliders = Physics.OverlapSphere(transform.position, m_ExplosionRadius);
+
+            for (int i = 0; i < colliders.Length; i++)
+            {
+                Debug.Log("JAI touché " + colliders[i].name);
+                Rigidbody targetRigidbody = colliders[i].GetComponent<Rigidbody>();
+                Rigidbody parentTry = colliders[i].GetComponentInParent<Rigidbody>();
+
+                // Si le parent de l'element a pas de rigidbody c'est pas un bout du tank, donc on passe au suivant.
+                if (!parentTry)
+                    continue;
+                //Si on trouve pas le tag tank, on a rien a faire donc on continue
+                if (parentTry.tag != "Tank")
+                    continue;
+
+                PilotController scriptpilote = parentTry.GetComponent<PilotController>();
+                if (scriptpilote)
+                {
+                    if (colliders[i].name.Contains("L_"))
+                    {
+                        if (Random.Range(0, 5) == 0)
+                            scriptpilote.DisableLeft();
+                    }
+                    if (colliders[i].name.Contains("R_"))
+                    {
+                        if (Random.Range(0, 5) == 0)
+                        {
+                            scriptpilote.DisableRight();
+                        }
+                    }
+                }
+
+                TankHealth targethealth = parentTry.GetComponent<TankHealth>();
+                if (!targethealth)
+                    continue;
+                //on met 5 dégats par partie du tank qu'on trouve, ce qui permet de scale les dégats
+                targethealth.TakeDamage(5);
+            }
+
+            m_ExplosionParticles = Instantiate(ExplosionPrefab).GetComponent<ParticleSystem>();
+            m_ExplosionParticles.transform.position = transform.position;
+            m_ExplosionAudio = m_ExplosionParticles.GetComponent<AudioSource>();
+            m_ExplosionParticles.Play();
+            m_ExplosionAudio.Play();
+
+            // Once the particles have finished, destroy the gameobject they are on.
+            ParticleSystem.MainModule mainModule = m_ExplosionParticles.main;
+            Destroy(m_ExplosionParticles.gameObject, mainModule.duration);
+            // Destroy la mine.
+            Destroy(gameObject);
+        }
+        
+    }
+}
--- Assets/__Scripts/ShellExplosion.cs
+++ Assets/__Scripts/ShellExplosion.cs
@@ -2,9 +2,6 @@ using UnityEngine;
 
     public class ShellExplosion : MonoBehaviour
     {
-        public LayerMask m_TankMask;                        // Used to filter what the explosion affects, this should be set to "Players".
-        public float m_MaxDamage = 100f;                    // The amount of damage done if the explosion is centred on a tank.
-        public float m_ExplosionForce = 1000f;              // The amount of force added to a tank at the centre of the explosion.
         public float m_MaxLifeTime = 2f;                    // The time in seconds before the shell is removed.
         public float m_ExplosionRadius = 5f;                // The maximum distance away from the explosion tanks can be and are still affected.
         public GameObject ExplosionPrefab;
@@ -23,7 +20,7 @@ using UnityEngine;
         {
         //Debug.Log("JAI TRIGGER");
         // Collect all the colliders in a sphere from the shell's current position to a radius of the explosion radius.
-        Collider[] colliders = Physics.OverlapSphere (transform.position, m_ExplosionRadius, m_TankMask);
+        Collider[] colliders = Physics.OverlapSphere (transform.position, m_ExplosionRadius);
 
             // Go through all the colliders...
             for (int i = 0; i < colliders.Length; i++)
@@ -65,30 +62,8 @@ using UnityEngine;
             if (!targethealth)
                 continue;
 
+            //on met 5 dégats par partie du tank qu'on trouve, ce qui permet de scale les dégats
             targethealth.TakeDamage(5);
-
-            
-            /*    if (!targetRigidbody)
-                    continue;
-            Debug.Log("JAI TOUCHEY "+targetRigidbody.name);*/
-            // Add an explosion force.
-            //targetRigidbody.AddExplosionForce (m_ExplosionForce, transform.position, m_ExplosionRadius);
-
-            // If they don't have a rigidbody, go on to the next collider.// If they don't have a rigidbody, go on to the next collider.
-            //Todo: gerer la perte de hp ou quoi
-            /*
-                // Find the TankHealth script associated with the rigidbody.
-                TankHealth targetHealth = targetRigidbody.GetComponent<TankHealth> ();
-
-                // If there is no TankHealth script attached to the gameobject, go on to the next collider.
-                if (!targetHealth)
-                    continue;
-
-                // Calculate the amount of damage the target should take based on it's distance from the shell.
-                float damage = CalculateDamage (targetRigidbody.position);
-
-                // Deal this damage to the tank.
-                targetHealth.TakeDamage (damage);*/
         }
 
         m_ExplosionParticles = Instantiate(ExplosionPrefab).GetComponent<ParticleSystem>();
@@ -102,26 +77,5 @@ using UnityEngine;
             Destroy (m_ExplosionParticles.gameObject, mainModule.duration);
             // Destroy the shell.
             Destroy (gameObject);
-        }
-
-
-        private float CalculateDamage (Vector3 targetPosition)
-        {
-            // Create a vector from the shell to the target.
-            Vector3 explosionToTarget = targetPosition - transform.position;
-
-            // Calculate the distance from the shell to the target.
-            float explosionDistance = explosionToTarget.magnitude;
-
-            // Calculate the proportion of the maximum distance (the explosionRadius) the target is away.
-            float relativeDistance = (m_ExplosionRadius - explosionDistance) / m_ExplosionRadius;
-
-            // Calculate damage as this proportion of the maximum possible damage.
-            float damage = relativeDistance * m_MaxDamage;
-
-            // Make sure that the minimum damage is always 0.
-            damage = Mathf.Max (0f, damage);
-
-            return damage;
         }
     }
